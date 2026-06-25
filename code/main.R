059d9286255dc25c6642545e2b8c3f93748166f3
kelly-sovacool <kelly.sovacool@nih.gov>
2026-06-25 18:42:06 +0000

    feat: initialize CO app, env, & run script

--- code/main.R
+++ code/main.R
@@ -1,15 +1,15 @@
 library(argparse)
-devtools::load_all("code/hello")
+devtools::load_all("/code/hello")
 
 # parse CLI arguments
 parser <- ArgumentParser(description = "demo capsule")
 parser$add_argument(
-  "--hello_object",
+  "--name",
   type = "character",
   required = FALSE,
   default = "world"
 )
 args <- parser$parse_args()
 
-hello_message(args$hello_object)
+hello_message(args$name)
 message("hello version: ", packageVersion("hello"))
